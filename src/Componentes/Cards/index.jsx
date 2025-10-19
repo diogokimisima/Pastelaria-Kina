5a10816f5620a58ee2c87cf1994b683e96e33d04
dkimisima@gmail.com <dkimisima@gmail.com>
2025-10-19 13:10:53 -0300

    adicionando funcionalidade de zoom na imagem do cardápio com estilo modal

--- src/Componentes/Cards/index.jsx
+++ src/Componentes/Cards/index.jsx
@@ -1,16 +1,19 @@
 import "./style.css";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Cards = ({ produto }) => {
+    const [zoom, setZoom] = useState(false);
+
     return (
-        <motion.div
-            className="card"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1 }}
-        >
-            
-                <figure>
+        <>
+            <motion.div
+                className="card"
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+            >
+                <figure onClick={() => setZoom(true)} style={{ cursor: "zoom-in" }}>
                     <img src={produto.imagem} alt={produto.nome} />
                 </figure>
                 <div className="container-informacoes">
@@ -23,7 +26,13 @@ const Cards = ({ produto }) => {
                         <span>{new Intl.NumberFormat("pt-br", { style: "currency", currency: "BRL" }).format(produto.preco)}</span>
                     </div>
                 </div>
-        </motion.div>
+            </motion.div>
+            {zoom && (
+                <div className="modal-zoom" onClick={() => setZoom(false)}>
+                    <img src={produto.imagem} alt={produto.nome} className="img-zoom" />
+                </div>
+            )}
+        </>
     )
 }
 
